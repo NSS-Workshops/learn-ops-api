89a0313299b6f1a2404e9fd6ff181e9c8a66513f
Greg Korte <gregmkorte@gmail.com>
2026-01-08 11:05:58 -0600

    Adding all example tests and test frameworks for tests to be done by students

---
+++ LearningAPI/tests/__init__.py
@@ -0,0 +1,13 @@
+"""
+Tests for the LearningAPI application.
+
+Test files are organized by model/view being tested:
+- test_cohort.py: Cohort model tests
+- test_course.py: Course model tests
+- test_assessment.py: Assessment model tests
+- test_student_note.py: StudentNote model tests
+- test_book.py: Book model tests
+- test_capstone.py: Capstone model tests
+- test_project.py: Project model tests
+- test_team_maker_integration.py: Team maker view integration tests
+"""
